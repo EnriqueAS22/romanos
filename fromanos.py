f368c33f0d3d35c95256fc9a2050753e749f51f2
Enrique Alba <enalsa2222@gmail.com>
2024-12-11 20:03:24 +0100

    feat: Funcion descomponer (posicion, cifra) completada

--- fromanos.py
+++ fromanos.py
@@ -15,10 +15,45 @@ def a_romanos(valor: int) -> str:
             romano = clave
     return romano
 
+"""
+a_romanos(int)
 
+lista_traduciones = []
+- Para cada posicion, cifra de int, de atras hacia delante
+    - Valor = descomponer (posicion, cifra)
+    - valor_traducido = traducir (valor)
+    - añadir valor_raducido a lista_traducciones
 
-assert a_romanos(1) == 'I' , "numero incorrecto"
-assert a_romanos(500) == 'D', "numero incorrecto"
+- lista_traducciones_ordenada = darle la vuelta a lista_traducciones
+- concatenar lista_traducciones
+"""
 
-assert a_romanos(2) == 'II'
-assert a_romanos(200) == 'CC'
+def descomponer(posicion: int, cifra:str) -> int:
+    """
+    - Pasar cifra a numero entero (int)
+    - Devolver cifra * 10 ** posicion
+    """
+    return int(cifra) * 10 ** posicion
+
+def traducir(valor:int) -> str:
+    pass
+
+def a_romanos(numero: int) -> str:
+    lista_traducciones = []
+
+    numero_str = str(numero)
+    reves = numero_str[::-1]
+    
+    for posicion, cifra in enumerate(reves):
+        valor = descomponer(posicion, cifra)
+        valor_traducido = traducir(valor)
+        lista_traducciones.append(valor_traducido)
+
+    lista_traducciones_inversa = lista_traducciones.reverse() # Tambien se puede hacer -> lista_traducciones[::-1]
+
+    num_romanos = ""
+    for simbolo in lista_traducciones_inversa:
+        num_romanos += simbolo
+
+    return num_romanos
+
---
+++ test_formanos.py
@@ -0,0 +1,21 @@
+
+from fromanos import a_romanos, descomponer
+
+def test_simbolos_sencillos():  
+    assert a_romanos(1) == 'I' , "numero incorrecto"
+    assert a_romanos(500) == 'D', "numero incorrecto"
+
+def test_doble_repeticion():
+    assert a_romanos(2) == 'II'
+    assert a_romanos(200) == 'CC'
+
+def test_descomponer():
+    # Vot a probar el numero 1939, cadena "9391"
+    resultado = descomponer(0, "9")
+    assert resultado == 9
+    resultado = descomponer(1, "3")
+    assert resultado == 30
+    resultado = descomponer(2, "9")
+    assert resultado == 900
+    resultado = descomponer(3, "1")
+    assert resultado == 1000
