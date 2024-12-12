7707e3dad737440f9db9ac4c393e5009a10ae2dd
Enrique Alba <enalsa2222@gmail.com>
2024-12-12 12:10:06 +0100

    feat: funcion traducir diccionario de todos los valores por unidades, decenas, centenas y millares

--- test_formanos.py
+++ test_formanos.py
@@ -1,11 +1,11 @@
 
-from fromanos import a_romanos, descomponer
+from fromanos import a_romanos, descomponer, traducir
 
-def test_simbolos_sencillos():  
+def _test_simbolos_sencillos():  
     assert a_romanos(1) == 'I' , "numero incorrecto"
     assert a_romanos(500) == 'D', "numero incorrecto"
 
-def test_doble_repeticion():
+def _test_doble_repeticion():
     assert a_romanos(2) == 'II'
     assert a_romanos(200) == 'CC'
 
@@ -19,3 +19,9 @@ def test_descomponer():
     assert resultado == 900
     resultado = descomponer(3, "1")
     assert resultado == 1000
+
+def test_traducir():
+    assert traducir(9) == "IX"
+    assert traducir(30) == "XXX"
+    assert traducir(900) == "CM"
+    assert traducir(1000) == "M"
